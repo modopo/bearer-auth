fc1b2cd2c7602964efd3b72b42d24a4b9572b358
Jonathan Tsai <modopo@gmail.com>
2023-04-12 13:00:47 -0700

    removed unused modules

--- src/auth/models/users.js
+++ src/auth/models/users.js
@@ -4,8 +4,6 @@ require('dotenv').config();
 
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const { parse } = require('path');
-
 
 const userSchema = (sequelize, DataTypes) => {
   const model = sequelize.define('User', {
